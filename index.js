d8c49b1e6a8988b5e76164eac41d1bbf354b548d
nirsitha <80614281+nirsitha@users.noreply.github.com>
2021-10-08 12:15:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var http=require("http");
-const port=process.env.PORT||3000
+const PORT=process.env.PORT||3000;
 var serve=http.createServer(function(req,res){
     res.write("<div ><h2>NAME:R.NIRSITHA</h2> </div>")
     res.write("<h1>HELLO WORLD</h1>");
     res.end();
 });
-serve.listen(port);
+serve.listen(PORT);
 console.log("nirsi its working");
